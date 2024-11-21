df188c25135212b8ce9eade6a62ca3df4540f385
MohamedElgharbawi <mohamedkhaledmohamed291@gmail.com>
2024-11-21 20:42:36 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+let input = document.querySelector('input');
+function appendToDisplay(val) {
+    input.value += val;
+}
+function calculate() {
+    try {
+        if (input.value) 
+            input.value = eval(input.value);
+        else
+            input.value = 'Error';
+    }
+    catch (error) {
+        input.value = "Error";
+    }   
+}
+function clearDisplay() {
+    input.value = "";
+}
+function deleteLastNum() {
+    if (input.value === "Error") 
+        input.value = "";
+    else 
+        input.value = input.value.slice(0, input.value.length - 1);
+}
